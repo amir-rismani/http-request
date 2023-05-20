700bf01f568a9ea3d3de7485b145013037534cf6
Amir Rismaani <amir.rismaani@gmail.com>
2023-05-20 21:51:41 +0330

    Use axios interceptors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,22 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.request.use((request) => {
+    console.log('request', request);
+    return request;
+}, (error) => {
+    console.log('error', error);
+    return Promise.reject();
+});
+
+axios.interceptors.response.use((response) => {
+    console.log('response', response);
+    return response;
+}, (error) => {
+    console.log('error', error);
+    return Promise.reject();
+});
+
 const http = {
     post: axios.post,
     get: axios.get,
