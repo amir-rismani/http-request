70eda2c1cf92fefa6cf0188a70bab6c49e32d115
Amir Rismaani <amir.rismaani@gmail.com>
2023-05-28 06:01:16 +0330

    404 not found page

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import AddComment from "./pages/AddComment/AddComment";
 import CommentDetails from "./pages/CommentDetails/CommentDetails";
-
+import NotFound from "./pages/NotFound/NotFound";
 const WebRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/add-comment" element={<AddComment />} />
             <Route path="/show-comment/:id" element={<CommentDetails />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
